2294ca04be0d4aac841e9c75c0c73b619924a0a1
LilParker <bgray888@comcast.net>
2013-06-23 19:04:29 -0700

    Added more characters to encryption array

--- RadioPlugin/src/io/github/LilParker/RadioPlugin/RadioPluginCommandExecuter.java
+++ RadioPlugin/src/io/github/LilParker/RadioPlugin/RadioPluginCommandExecuter.java
@@ -12,7 +12,9 @@ public class RadioPluginCommandExecuter implements CommandExecutor {
 	RadioPlugin plugin;
 	public HashMap<String, Float> playerFreqs = new HashMap<String, Float>();
 	public HashMap<String, String> eKey = new HashMap<String, String>();
-	private String[] alphanumeric = new String[]{"a","b","c","d","e","f","g","h","i","k","j","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",""};
+	private String[] alphanumeric = new String[]{"a","b","c","d","e","f","g","h","i","k","j","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+			"1","2","3","4","5","6","7","8","9","0",
+			"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
 	
 	public RadioPluginCommandExecuter (RadioPlugin actPlugin) {
 		plugin = actPlugin;
